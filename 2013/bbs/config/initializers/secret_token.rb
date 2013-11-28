959bcd5457f01ec55add0147cbab90d19e7d6a73
Koichiro Ohba <koichiro@meadowy.org>
2013-11-28 14:49:39 +0900

    Initial commit

---
+++ 2013/bbs/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bbs::Application.config.secret_token = '2d623e6346e4f982725fe961bdafd4e2f8aff2fe6f85a7486980202863ae0994193d1d2a3f64f159bbdfd43250fd022263572f1a3ad931a9362b88f312c078bb'
