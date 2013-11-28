b65351276117b0719bf588140f68f0f01bdd5d47
Koichiro Ohba <koichiro@meadowy.org>
2013-11-28 16:21:49 +0900

    Add view and action

--- 2013/bbs/app/controllers/entries_controller.rb
+++ 2013/bbs/app/controllers/entries_controller.rb
@@ -1,4 +1,17 @@
 class EntriesController < ApplicationController
   def index
+    @entries = Entry.all
+  end
+
+  def create
+    @entry = Entry.new(params[:entry])
+
+    respond_to do |format|
+      if @entry.save
+        format.html { redirect_to(:action => 'index', :notice => 'Task was successfully created.') }
+      else
+        format.html { render :action => "new" }
+      end
+    end
   end
 end
