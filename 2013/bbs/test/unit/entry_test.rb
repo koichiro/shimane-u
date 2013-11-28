19b69fb13cdd3cdaa2b618de5f1f67028f97e3b9
Koichiro Ohba <koichiro@meadowy.org>
2013-11-28 15:34:55 +0900

    Add entry test

--- 2013/bbs/test/unit/entry_test.rb
+++ 2013/bbs/test/unit/entry_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class EntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Empty content" do
+    entry = Entry.new(:user_name => "taro", :content => "")
+    assert_equal false, entry.save
+  end
 end
