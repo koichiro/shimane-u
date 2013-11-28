a1e8f52c3d10bf8fbeca5334adade9a07ff5e658
Koichiro Ohba <koichiro@meadowy.org>
2013-11-28 15:48:08 +0900

    Add entry route

--- 2013/bbs/config/routes.rb
+++ 2013/bbs/config/routes.rb
@@ -2,6 +2,8 @@ Bbs::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  resources :entries
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
